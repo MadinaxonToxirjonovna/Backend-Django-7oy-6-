39c9f85f908c49951d14203299a74d640439aa14
Madinaxon Toxirjonovna <madinaxontoxirjonovna@gamil.com>
2025-03-27 19:15:53 +0500

    7oy(7)

--- student/students/urls.py
+++ student/students/urls.py
@@ -1,10 +1,20 @@
+                                    # APIView
 from django.urls import path
-from .views import StudentList, StudentCreate, StudentRetrieve, StudentUpdate, StudentDelete
+# from .views import StudentList, StudentCreate, StudentRetrieve, StudentUpdate, StudentDelete
 
-urlpatterns = [
-    path('students/', StudentList.as_view(), name='student_list'),
-    path('students/create/', StudentCreate.as_view(), name='student_create'),
-    path('students/<int:pk>/', StudentRetrieve.as_view(), name='student_detail'),
-    path('students/update/<int:pk>/', StudentUpdate.as_view(), name='student_update'),
-    path('students/delete/<int:pk>/', StudentDelete.as_view(), name='student_delete'),
-]
+# urlpatterns = [
+#     path('students/', StudentList.as_view(), name='student_list'),
+#     path('students/create/', StudentCreate.as_view(), name='student_create'),
+#     path('students/<int:pk>/', StudentRetrieve.as_view(), name='student_detail'),
+#     path('students/update/<int:pk>/', StudentUpdate.as_view(), name='student_update'),
+#     path('students/delete/<int:pk>/', StudentDelete.as_view(), name='student_delete'),
+# ]
+
+
+                                     # viewsets 
+from .views import StudentViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'students', StudentViewSet, basename='student')
+urlpatterns = router.urls
--- student/students/views.py
+++ student/students/views.py
@@ -1,32 +1,61 @@
-from django.shortcuts import render
-from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, UpdateAPIView, RetrieveAPIView
+                                         # APIView
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+# from django.shortcuts import get_object_or_404
+# from .models import Student
+# from .serializers import StudentSerializer
+
+# class StudentList(APIView):
+#     def get(self, request):
+#         students = Student.objects.all()
+#         serializer = StudentSerializer(students, many=True)
+#         return Response({"data": serializer.data, "status": status.HTTP_200_OK, "message": "Students List"})
+
+# class StudentCreate(APIView):
+#     def post(self, request):
+#         serializer = StudentSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"data": serializer.data, "status": status.HTTP_201_CREATED, "message": "Student Created"})
+#         return Response({"errors": serializer.errors, "status": status.HTTP_400_BAD_REQUEST})
+
+# class StudentRetrieve(APIView):
+#     def get(self, request, pk):
+#         student = get_object_or_404(Student, pk=pk)
+#         serializer = StudentSerializer(student)
+#         return Response({"data": serializer.data, "status": status.HTTP_200_OK, "message": "Student Details"})
+
+# class StudentUpdate(APIView):
+#     def put(self, request, pk):
+#         student = get_object_or_404(Student, pk=pk)
+#         serializer = StudentSerializer(student, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"data": serializer.data, "status": status.HTTP_200_OK, "message": "Student Updated"})
+#         return Response({"errors": serializer.errors, "status": status.HTTP_400_BAD_REQUEST})
+
+#     def patch(self, request, pk):
+#         student = get_object_or_404(Student, pk=pk)
+#         serializer = StudentSerializer(student, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"data": serializer.data, "status": status.HTTP_200_OK, "message": "Student Partially Updated"})
+#         return Response({"errors": serializer.errors, "status": status.HTTP_400_BAD_REQUEST})
+
+# class StudentDelete(APIView):
+#     def delete(self, request, pk):
+#         student = get_object_or_404(Student, pk=pk)
+#         student.delete()
+#         return Response({"message": "Student Deleted"}, status=status.HTTP_204_NO_CONTENT)
+
+
+                                      # viewsets 
 from.models import Student
 from .serializers import StudentSerializer
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import viewsets
 
-class StudentList(ListAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-
-
-class StudentCreate(CreateAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-
-class StudentRetrieve(RetrieveAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-    lookup_field = 'pk'
-
-class StudentUpdate(UpdateAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-    lookup_field = 'pk'
-
-
-class StudentDelete(DestroyAPIView):
+class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     lookup_field = 'pk'
